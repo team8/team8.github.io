7b9db0690b60d91d0babe8ccd57d785ef44035be
Alecajuice <alecadad@gmail.com>
2013-12-07 15:58:40 -0800

    Got rid of html in a js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -102,17 +102,8 @@ $(function () {
 /*****************************************************************************/
 
 
-<script type="text/javascript" 
-src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js">
-</script>
-<script type="text/javascript" src="jquery.simplyscroll.js"></script>
-<link rel="stylesheet" href="jquery.simplyscroll.css" media="all" 
-type="text/css">
-
-<script type="text/javascript">
 (function($) {
 	$(function() { //on DOM ready 
     		$("#scroller").simplyScroll();
 	});
  })(jQuery);
-</script>
