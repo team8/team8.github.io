0acec53c94b8bde2307db0f3e6d94a51784d14f7
JonathanZwiebel <jonathanzwiebel@gmail.com>
2013-12-07 15:16:56 -0800

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -82,7 +82,7 @@ $(function () {
                 animate_img();
             });
         } else {
-            $image.animate({left: '0px'}, 1000, function () {
+            $image.animate({left: '1000px'}, 1000, function () {
                 $image.css('left','-1000px');
                 animate_img();
             });
