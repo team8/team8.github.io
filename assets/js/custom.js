54f6dba2063335f66248e4ea0d44a8f97fc258cc
JonathanZwiebel <jonathanzwiebel@gmail.com>
2013-12-07 15:47:03 -0800

    Infinite Image Attempt 2 JS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -90,3 +90,29 @@ $(function () {
     }
     animate_img();
 });
+
+
+/*****************************************************************************/
+/*                                                                                                               
+
+/* Infinite Image Attempt 2                                                                                                 
+
+/*                                                                                                                
+
+/*****************************************************************************/
+
+
+<script type="text/javascript" 
+src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js">
+</script>
+<script type="text/javascript" src="jquery.simplyscroll.js"></script>
+<link rel="stylesheet" href="jquery.simplyscroll.css" media="all" 
+type="text/css">
+
+<script type="text/javascript">
+(function($) {
+	$(function() { //on DOM ready 
+    		$("#scroller").simplyScroll();
+	});
+ })(jQuery);
+</script>
