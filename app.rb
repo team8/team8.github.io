a62de56ff94b782bb0d61b9ba43d58e498040750
Maxwell Bernstein <tekknolagi@gmail.com>
2014-01-11 19:40:17 +0000

    do i even need the route?

--- app.rb
+++ app.rb
@@ -10,11 +10,11 @@ before do
   response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
 end
 
-get '/*' do
-  file_name = "_site#{request.path_info}/index.html".gsub(%r{\/+},'/')
-  if File.exists?(file_name)
-    File.read(file_name)
-  else
-    raise Sinatra::NotFound
-  end
-end
+#get '/*' do
+#  file_name = "_site#{request.path_info}/index.html".gsub(%r{\/+},'/')
+#  if File.exists?(file_name)
+#    File.read(file_name)
+#  else
+#    raise Sinatra::NotFound
+#  end
+#end
